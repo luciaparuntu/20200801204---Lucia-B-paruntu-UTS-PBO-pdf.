ba7ebb3b9ffa7747863497eb06ba61f5a767bf57
luciaparuntu <91532236+luciaparuntu@users.noreply.github.com>
2021-11-11 17:40:35 +0700

    20200801204 - Lucia B Paruntu UTS PBO

---
+++ utsno5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, _, i, _;
+
+    cout<<"masukan jumlah baris : ";
+    cin>>n;
+    for(i=i;i<=n;i++){
+        for(s=1;s<n;s++)
+        cout<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
